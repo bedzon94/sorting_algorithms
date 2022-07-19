4b0d4795e26370c2cdbd96495eb156f37f983d2d
Norris <99494922+bedzon94@users.noreply.github.com>
2022-07-19 18:08:42 -0400

    106-bitonic_sort.c

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -10,8 +10,6 @@ void bitonic_sort(int *array, size_t size);
  * swap_ints - Swap two integers in an array.
  * @a: The first integer to swap.
  * @b: The second integer to swap.
- *
- * Authors: Lawrence Adams & Francesca Lynn Asiedu Asare
  */
 void swap_ints(int *a, int *b)
 {
